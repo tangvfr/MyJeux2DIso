fd220187b7b31a752a7a4fc4ff458c1d1ba61b98
Tangvfr <tangv.minecraft@gmail.com>
2019-04-11 16:43:38 +0200

    .

--- src/fr/tangv/jeux2diso/objets/ColideMask.java
+++ src/fr/tangv/jeux2diso/objets/ColideMask.java
@@ -6,15 +6,6 @@ public interface ColideMask {
 
 	public static ColideMask rectangle = new ColideMask() {
 		@Override
-		public boolean colide(Colide my, Colide colide) {
-			if (colide.getMaxX() < my.getMinX()) return false;
-			if (colide.getMaxY() < my.getMinY()) return false;
-			if (colide.getMinX() >= my.getMaxX()) return false;
-			if (colide.getMinY() >= my.getMaxY()) return false;
-			return true;
-		}
-		
-		@Override
 		public boolean[][] getMask(int width, int height) {
 			boolean[][] mask = new boolean[width][height];
 			for (int x = 0; x < width; x++) for (int y = 0; y < height; y++) {
@@ -27,21 +18,26 @@ public interface ColideMask {
 	
 	public static ColideMask block = new ColideMask() {
 		@Override
-		public boolean colide(Colide my, Colide colide) {
-			my.get
-			//░▒▓
-			
-			return true;
-		}
-		
-		@Override
 		public boolean[][] getMask(int width, int height) {
 			return Mask.maskblockfull;
 		}
-		
 	};
 	
-	public boolean colide(Colide my, Colide colide);
+	public default boolean colide(Colide my, Colide colide) {
+		int maxx =	(int) Math.max(my.getMaxX(), colide.getMaxX());
+		int maxy = (int) Math.max(my.getMaxY(), colide.getMaxY());
+		int minx = (int) Math.min(my.getMinX(), colide.getMinX());
+		int miny =  (int) Math.min(my.getMinY(), colide.getMinY());
+		/*int width = ;
+		int height = ;
+		boolean[][] talcomp = new boolean[][];*/
+		
+		
+		//░▓█
+		
+		return true;
+	};
+	
 	public boolean[][] getMask(int width, int height);
 	
 }
