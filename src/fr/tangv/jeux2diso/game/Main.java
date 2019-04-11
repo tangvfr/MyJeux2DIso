092c486ed0f09e7b92f771797958d86c15021177
Tangvfr <tangv.minecraft@gmail.com>
2019-04-11 13:18:27 +0200

    .

--- src/fr/tangv/jeux2diso/game/Main.java
+++ src/fr/tangv/jeux2diso/game/Main.java
@@ -6,6 +6,8 @@ public class Main {
 	
 	//comencé le 08/04/2019
 	public static byte first = 0;
+	public static final String rep = System.getenv("APPDATA")+"\\.Myjeuxtest\\";
+	public static String repparametre;
 	
 	public static void sendConsol(String string) {
 		System.out.println("[Jeux2DIso] "+string);
@@ -13,6 +15,8 @@ public class Main {
 	
 	public static void main(String args[]) {
 		try {
+			repparametre = rep+"\\para";
+			sendConsol(rep);
 			new App();
 		} catch (SlickException e) {
 			e.printStackTrace();
--- src/fr/tangv/jeux2diso/tools/Parametre.java
+++ src/fr/tangv/jeux2diso/tools/Parametre.java
@@ -1,12 +1,40 @@
 package fr.tangv.jeux2diso.tools;
 
-public class Parametre implements Loading {
+import java.io.IOException;
+import java.util.Map;
+
+import org.newdawn.slick.util.ResourceLoader;
+import org.simpleyaml.configuration.file.YamlFile;
+import org.simpleyaml.configuration.serialization.ConfigurationSerializable;
+import org.simpleyaml.exceptions.InvalidConfigurationException;
+
+import fr.tangv.jeux2diso.game.Main;
+
+public class Parametre implements Loading, ConfigurationSerializable {
 	
 	public static final Parametre parametre = new Parametre();
+	
+	private YamlFile yfile = new YamlFile(Main.repparametre);
+	
+	@Override
+	public Map<String, Object> serialize() {
+		
+		return null;
+	}
 
+	@SuppressWarnings("deprecation")
 	@Override
 	public void ini() {
-		//faire le system de config
+		try {
+			if (yfile.exists()) {
+				yfile.load();
+			} else {
+				yfile.load(ResourceLoader.getResourceAsStream("res/para/para"));
+				yfile.createNewFile(true);
+			}
+		} catch (InvalidConfigurationException | IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
