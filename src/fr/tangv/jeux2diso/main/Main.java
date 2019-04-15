62fd182997a74ac6edfc0bc5eb5e00bb064fc357
Tangvfr <tangv.minecraft@gmail.com>
2019-04-15 18:48:20 +0200

    .

--- src/fr/tangv/jeux2diso/main/Main.java
+++ src/fr/tangv/jeux2diso/main/Main.java
@@ -18,7 +18,7 @@ public class Main {
 	public static String rep = System.getenv("APPDATA")+"/";
 	public static String repparametre;
 	public static String replangue;
-	public static final String version = "alpha_1.1";
+	public static final String version = "alpha_1.2";
 	public static final String namegame = "Numiota";
 	
 	public static boolean low;
